b3f278bd5a636d63116753bad01cbfe4c943ac93
MalarJarold <malarjm27@gmail.com>
2021-10-20 10:48:35 +0530

    msg added

--- src/com/gitsam/GitPgm.java
+++ src/com/gitsam/GitPgm.java
@@ -1,5 +1,15 @@
 package com.gitsam;
 
 public class GitPgm {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		
+		System.out.println(7);
+		System.out.println(4);
+		System.out.println(5);
+	}
 
 }
